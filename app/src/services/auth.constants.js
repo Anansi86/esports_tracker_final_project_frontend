4b7e8e062208f62fb75c468343fc132f70a35edd
Anansi86 <shawn.staton@student.awesomeinc.org>
2022-12-01 21:50:18 +0000

    authentication

---
+++ app/src/services/auth.constants.js
@@ -0,0 +1,4 @@
+export const API_URL = "https://8000-anansi86-esportstracker-wd0y3cht4zg.ws-us77.gitpod.io/match/?format=json";
+export const LOGIN_ENDPOINT = "user/login/"
+export const REGISTER_ENDPOINT = "user/signup/"
+export const REFRESH_ENDPOINT = "token/refresh/"
