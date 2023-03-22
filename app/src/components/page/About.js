c79e861f9179663989b91cfdeb37edcd79d923cc
Anansi86 <shawn.staton@student.awesomeinc.org>
2023-03-22 18:08:39 +0000

    fixed responsiveness on team page

--- app/src/components/page/About.js
+++ app/src/components/page/About.js
@@ -21,11 +21,27 @@ const About = () => {
     }, []);
     
     return (
+      
+
+
       <div className="container">
         <div className="row">
-          {heroes.map((hero) => {
-            return (
-              <div class="col p-3 d-flex flex-column position-static">
+    <div className="col">
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">About OverWatch</h5>
+          <p className="card-text">Overwatch 2 is a free-to-play, team-based action game set in the optimistic future, 
+          where every match is the ultimate 5v5 battlefield brawl. 
+          Play as a time-jumping freedom fighter, a beat-dropping battlefield DJ, 
+          or one of over 30 other unique heroes as you battle it out around the globe.</p>
+          {/* <p></p> */}
+          {/* <Link to="/Login" className="btn btn-primary">Click Here</Link> */}
+        </div>
+      </div>
+    </div>
+        <div className="row">
+          {heroes.map((hero) => (
+              <div className="col p-3 d-flex flex-column position-static">
                 <div className="card h-100">
                   <div className="card-body ">
                     <h5 className="card-title">{hero.name}</h5>
@@ -35,10 +51,11 @@ const About = () => {
                   </div>
                 </div>
               </div>
-            );
-          })}
+            )
+          )}
         </div>
+      </div>
       </div>
     );
         }
-export default About
+export default About;
