1781b1050c0ce923ebb09a5def911be97a4f4570
Anansi86 <shawn.staton@student.awesomeinc.org>
2023-03-22 18:16:35 +0000

    resized team icons

--- app/src/components/page/About.js
+++ app/src/components/page/About.js
@@ -3,59 +3,61 @@ import { Link } from "react-router-dom";
 import request from "../../services/api.request";
 
 const About = () => {
-  // let navigate = useNavigate();
-    const [heroes, setHeroes] = useState([]);
-  
-    useEffect(() => {
-      async function getData() {
-        const responseHeroes = await request({
-          method: "GET",
-          url: "Hero/",
-        });
-  
-        setHeroes(responseHeroes.data);
-        console.log(responseHeroes.data)
-      }
-      getData();
-      
-    }, []);
-    
-    return (
-      
+  const [heroes, setHeroes] = useState([]);
 
+  useEffect(() => {
+    async function getData() {
+      const responseHeroes = await request({
+        method: "GET",
+        url: "Hero/",
+      });
 
-      <div className="container">
+      setHeroes(responseHeroes.data);
+      //  console.log(responseHeroes.data)
+    }
+    getData();
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">About OverWatch</h5>
+              <p className="card-text">
+                Overwatch 2 is a free-to-play, team-based action game set in the
+                optimistic future, where every match is the ultimate 5v5
+                battlefield brawl. Play as a time-jumping freedom fighter, a
+                beat-dropping battlefield DJ, or one of over 30 other unique
+                heroes as you battle it out around the globe.
+              </p>
+              {/* <p></p> */}
+              {/* <Link to="/Login" className="btn btn-primary">Click Here</Link> */}
+            </div>
+          </div>
+        </div>
         <div className="row">
-    <div className="col">
-      <div className="card">
-        <div className="card-body">
-          <h5 className="card-title">About OverWatch</h5>
-          <p className="card-text">Overwatch 2 is a free-to-play, team-based action game set in the optimistic future, 
-          where every match is the ultimate 5v5 battlefield brawl. 
-          Play as a time-jumping freedom fighter, a beat-dropping battlefield DJ, 
-          or one of over 30 other unique heroes as you battle it out around the globe.</p>
-          {/* <p></p> */}
-          {/* <Link to="/Login" className="btn btn-primary">Click Here</Link> */}
+          {heroes.map((hero) => (
+            <div className="col p-3 d-flex flex-column position-static">
+              <div className="card h-100">
+                <div className="card-body ">
+                  <h5 className="card-title">{hero.name}</h5>
+                  <Link to={"/Character/" + hero.id}>
+                    <img
+                      className="hero_icon"
+                      href="Home"
+                      src={hero.character_img}
+                      alt="hero_icon"
+                    />
+                  </Link>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
-        <div className="row">
-          {heroes.map((hero) => (
-              <div className="col p-3 d-flex flex-column position-static">
-                <div className="card h-100">
-                  <div className="card-body ">
-                    <h5 className="card-title">{hero.name}</h5>
-                      <Link to={"/Character/" + hero.id}>
-                      <img className="hero_icon" href="Home" src={hero.character_img} alt="hero_icon" />
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            )
-          )}
-        </div>
-      </div>
-      </div>
-    );
-        }
+  );
+};
 export default About;
--- app/src/components/page/Team_Selection.js
+++ app/src/components/page/Team_Selection.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import request from "../../services/api.request";
 
-
 const Team_Selection = () => {
   const [teams, setTeams] = useState([]);
 
@@ -14,44 +13,49 @@ const Team_Selection = () => {
       });
 
       setTeams(responseTeam.data);
-      console.log(responseTeam.data)
+      // console.log(responseTeam.data)
     }
     getData();
-    
   }, []);
 
   return (
-  <div className="row">
-  {teams.map((team) => (
-      <div class="col p-3 d-flex flex-column position-static">
-        <div className="card h-100">
-          <div className="card-body">
-            <h5 className="card-title">{team.name}</h5>
+  <div className="container">
+    <div className="row">
+      {teams.map((team) => (
+        <div class="col p-3 d-flex flex-column position-static">
+          <div className="card h-100">
+            <div className="card-body">
+              <h5 className="card-title">{team.name}</h5>
               <Link to={"/Team/" + team.id}>
-              <img className="team_icon" href="Home" src={team.icon} alt="team_icon" />
-            </Link>
+                <img
+                  className="team_icon"
+                  href="Home"
+                  src={team.icon}
+                  alt="team_icon"
+                  />
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
-    )
-  )}
-</div>
-      // <div className="row">
-      //   {teams.map((team) => {
-      //     return (
-      //       <div className="col-3">
-      //         <div className="card ">
-      //           <div className="card-body">
-      //             <h5 className="card-title">{team.name}</h5>
-      //               <Link to={"/Team/" + team.id}>
-      //               <img className="team_icon" href="Home" src={team.icon} alt="team_icon" />
-      //             </Link>
-      //           </div>
-      //         </div>
-      //       </div>
-      //     );
-      //   })}
-      // </div>
-  )
-    };
-export default Team_Selection;
+      ))}
+    </div>
+  </div>
+    );
+  };
+  export default Team_Selection;
+    // <div className="row">
+    //   {teams.map((team) => {
+    //     return (
+    //       <div className="col-3">
+    //         <div className="card ">
+    //           <div className="card-body">
+    //             <h5 className="card-title">{team.name}</h5>
+    //               <Link to={"/Team/" + team.id}>
+    //               <img className="team_icon" href="Home" src={team.icon} alt="team_icon" />
+    //             </Link>
+    //           </div>
+    //         </div>
+    //       </div>
+    //     );
+    //   })}
+    // </div>
