435264b734639cda9dfc7532ff8b8e599082b37b
Anansi86 <shawn.staton@student.awesomeinc.org>
2023-03-23 17:06:34 +0000

    fixed team page responsiveness

--- app/src/components/page/Team_Selection.js
+++ app/src/components/page/Team_Selection.js
@@ -20,23 +20,23 @@ const Team_Selection = () => {
 
   return (
   <div className="container">
-    <div className="row">
-      {teams.map((team) => (
-        <div class="col p-3 d-flex flex-column position-static">
-          <div className="card h-100">
-            <div className="card-body">
-              <h5 className="card-title">{team.name}</h5>
-              <Link to={"/Team/" + team.id}>
-                <img
-                  className="team_icon"
-                  href="Home"
-                  src={team.icon}
-                  alt="team_icon"
-                  />
-              </Link>
+      <div className="row">
+        {teams.map((team) => (
+          <div class="col p-3 d-flex flex-column position-static">
+            <div className="card-team">
+              <div className="card-body">
+                <h5 className="card-title">{team.name}</h5>
+                <Link to={"/Team/" + team.id}>
+                  <img
+                    className="team_icon_team"
+                    href="Home"
+                    src={team.icon}
+                    alt="team_icon"
+                    />
+                </Link>
+              </div>
             </div>
           </div>
-        </div>
       ))}
     </div>
   </div>
